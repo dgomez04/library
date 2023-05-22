81dcef8c9ed5d13e17ca6a224ef7cfb4697b0fb3
dgomez04 <dagomezmoreno@outlook.com>
2023-05-22 08:34:38 -0600

    created constructor and started working on addBook function

--- library.js
+++ library.js
@@ -1,1 +1,16 @@
-adsa;
+/* eslint-disable no-unused-vars */
+// array to store books
+const myLibrary = [];
+
+// constructor
+function Book(author, title, pages) {
+  this.author = author;
+  this.title = title;
+  this.pages = pages;
+}
+
+// function to add book to array
+
+function addBookToLibrary() {
+
+}
