325e980203a8d316b9f3aff4d22eb4c7c8c9f70c
dgomez04 <dagomezmoreno@outlook.com>
2023-05-24 23:46:32 -0600

    started layout and created modal

--- library.js
+++ library.js
@@ -1,6 +1,23 @@
 /* eslint-disable no-unused-vars */
+
+// creating DOM objects to control pop-up
+const open = document.getElementById('open');
+const close = document.getElementById('close');
+const modalcontainer = document.getElementById('modalcontainer');
+
 // array to store books
 const myLibrary = [];
+
+// eventListeners for open & close buttons
+
+open.addEventListener('click', () => {
+  modalcontainer.classList.add('show');
+});
+
+close.addEventListener('click', () => {
+  // TODO: run function to add book
+  modalcontainer.classList.remove('show');
+});
 
 // constructor
 function Book(author, title, pages) {
