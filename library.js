5753a051deeecb7ea676825a0561350e00603d17
dgomez04 <dagomezmoreno@outlook.com>
2023-05-25 18:37:28 -0600

    finished function to add books to array

--- library.js
+++ library.js
@@ -2,11 +2,36 @@
 
 // creating DOM objects to control pop-up
 const open = document.getElementById('open');
-const close = document.getElementById('close');
+const submit = document.getElementById('submit');
 const modalcontainer = document.getElementById('modalcontainer');
+
+// creating DOM objects for input values.
+const bookInput = document.getElementById('book');
+const authorInput = document.getElementById('author');
+const pagesInput = document.getElementById('pages');
 
 // array to store books
 const myLibrary = [];
+
+// constructor
+function Book(book, author, pages) {
+  this.book = book;
+  this.author = author;
+  this.pages = pages;
+  // eslint-disable-next-line no-undef
+  isRead = false;
+}
+
+// function to add book to array
+
+function addBookToLibrary() {
+  myLibrary.push(new Book(bookInput.value, authorInput.value, pagesInput.value));
+
+  // clear values of input tags
+  bookInput.value = '';
+  authorInput.value = '';
+  pagesInput.value = '';
+}
 
 // eventListeners for open & close buttons
 
@@ -14,20 +39,7 @@ open.addEventListener('click', () => {
   modalcontainer.classList.add('show');
 });
 
-close.addEventListener('click', () => {
-  // TODO: run function to add book
+submit.addEventListener('click', () => {
   modalcontainer.classList.remove('show');
+  addBookToLibrary();
 });
-
-// constructor
-function Book(author, title, pages) {
-  this.author = author;
-  this.title = title;
-  this.pages = pages;
-}
-
-// function to add book to array
-
-function addBookToLibrary() {
-
-}
